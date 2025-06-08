28716da0ee78cce59b79245c2924a4632bd3025e
Pongsapat1035 <pongsapat357@gmail.com>
2025-06-08 21:21:05 +0700

    add docker file and test deploy

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,15 @@ import cookieParser from 'cookie-parser'
 import authRoute from './routes/auth.route'
 import bookmarkRoute from './routes/bookmark.route'
 import whitelistRoute from './routes/whitelist.route'
+import { Request, Response } from 'express'
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+app.get('/connect', (req: Request, res: Response) => {
+    res.status(200).json({ message: "ok" })
+})
 
 app.use('/auth', authRoute)
 app.use('/bookmark', bookmarkRoute)
